1294ae2fdfeba8201743d43045d1b65ca37fcbcf
Peter Evance <peterevance1@gmail.com>
2023-07-01 06:03:54 +0300

    URL Fetching...

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,12 @@
-#!/usr/bin/python3
 import urllib.request
 
 url = "https://alx-intranet.hbtn.io/status"
 
 with urllib.request.urlopen(url) as response:
-    body = response.read().decode('utf-8')
+    body = response.read()
+    body_str = body.decode('utf-8')
 
-print("- Body response:")
-print("\t- type:", type(body))
-print("\t- content:", body)
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(body_str))
